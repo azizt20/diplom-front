4714fde8f2e18b38155b520a24a9611cd2289179
Азиз <aziiz0120@gmail.com>
2022-06-10 17:33:39 +0500

    first commit

---
+++ src/views/CheckoutView.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "CheckoutView"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/OrderView.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "OrderView"
+}
+</script>
+
+<style scoped>
+
+</style>
